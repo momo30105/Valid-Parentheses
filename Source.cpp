10aa3b9251c8f2ca6d17b85dd9c2dcde11ebee85
momo <s04353041@go.thu.edu.tw>
2018-10-04 17:54:18 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,63 @@
+	 #include <iostream>
+	 //#include <string>
+	 #include <stack>
+	 // Enable STL stack.
+	 using namespace std;
+
+ int main(int argc, char *argv[]) { //程式進入點
+	
+	 char input[50];
+	 cin >> input; 
+	 stack<char> S; 
+	
+		
+		 for (int i = 0; i < (sizeof(input) / sizeof(input[0])); i++) {
+		 if (input[i] == '\0') { //End line.
+			 return 0;
+			
+		}
+		else if (input[i] == '(') { //First match
+			 S.push(input[i]);
+			
+		}
+		else if (input[i] == '[') { //Second match
+			 S.push(input[i]);
+			
+		}
+		else if (input[i] == '{') { //Third match
+			 S.push(input[i]);
+			
+		}
+		else {
+			
+				 if (S.size() != 0) {
+				 if ((S.top() == '(') && (input[i] == ')')) { //First match
+					 cout << S.top() << input[i] << endl;
+					 S.pop();
+					
+				}
+				else if ((S.top() == '[') && (input[i] == ']')) { //Second match
+					 cout << S.top() << input[i] << endl;
+					 S.pop();
+					
+				}
+				else if ((S.top() == '{') && (input[i] == '}')) { //Third match
+					 cout << S.top() << input[i] << endl;
+					 S.pop();
+					
+				}
+				
+			}
+				else {
+					 cout << "Inpossible error." << endl;
+					 break;
+				
+				}
+				
+		}
+		
+	}
+
+	 return 0; //程式結束點
+	
+}
